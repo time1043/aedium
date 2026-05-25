95a27d5870f7896e2c226526b3b42f6a5e82bff9
oswin902m <1665434994@qq.com>
2026-05-25 16:00:17 +0800

    chore(init): setup blocknote

--- src/routes/_main/articles/draft.tsx
+++ src/routes/_main/articles/draft.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import Editor from '@/components/Editor';
+
 export const Route = createFileRoute('/_main/articles/draft')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/_main/articles/draft"!</div>;
+  return <Editor />;
 }
