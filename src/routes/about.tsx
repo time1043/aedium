8aee06b8fcd5f6314a38b3c909d3060a46fe9b89
oswin902m <1665434994@qq.com>
2026-05-25 08:29:30 +0800

    chore(init): setup daisyui

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,8 +1,8 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/about')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return <div className="p-2">Hello from About!</div>;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
